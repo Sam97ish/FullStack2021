d532743a6da008a632da245036d3c5a2b4e08281
Sam <ha223cz@student.lnu.se>
2021-10-18 09:21:12 +0300

    uibuild

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -123,6 +123,7 @@ app.put('/api/persons/:id',  (request, response, next) => {
     number: body.number,
   }
 
+  
   Person.findByIdAndUpdate(request.params.id, person, { new: true, runValidators: true })
     .then(updatedPerson => {
       response.json(updatedPerson)
