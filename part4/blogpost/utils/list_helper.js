fb11ce758508ba0d0e24c3822ffec0cdc3948570
Sam <ha223cz@student.lnu.se>
2021-10-20 15:04:49 +0300

    started testing

---
+++ part4/blogpost/utils/list_helper.js
@@ -0,0 +1,29 @@
+// eslint-disable-next-line no-unused-vars
+const dummy = (blogs) => {
+  return 1
+}
+
+const totalLikes  = (blogs) => {
+  const reducer = (sum, blog) => {return sum + blog.likes}
+  return blogs.length === 0
+    ? 0
+    : blogs.reduce(reducer, 0)
+}
+
+const favoriteBlog = (blogs) => {
+  const reducer = (sum, blog) => {return sum + blog.likes}
+
+  let maxLike = blogs.length === 0
+    ? 0
+    : blogs.map()
+
+  return blogs.length === 0
+    ? 0
+    : blogs.reduce(reducer, 0)
+}
+
+module.exports = {
+  dummy,
+  totalLikes,
+  favoriteBlog
+}
